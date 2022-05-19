13be73703e54fbc6ed2421a8c51ebf779f145059
Julian Dockendorf <julian.dockendorf@stud-mail.uni-wuerzburg.de>
2022-05-20 01:26:32 +0200

    flashlight extended and new renderer HDRP

---
+++ Assets/Flashlights/flashlight_MrLarodos/flashlight/flashlight_slow_parent.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:064c443acfb7a97c84b4ba8bc157ca70187341765085e46deb07a23cc8a7faf1
+size 6059
---
+++ Assets/Flashlights/flashlight_MrLarodos/flashlight_slow_parent.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b88bd7a96a63b4f262f5f8348d785dfc6ae902438819433a0057e7ce9bfacec4
+size 5921
