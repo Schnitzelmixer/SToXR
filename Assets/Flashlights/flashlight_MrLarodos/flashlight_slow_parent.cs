70bc68ee49010ad8b19750cb6c510be156552a66
Mona Röhm <mona.roehm@protonmail.com>
2022-06-05 17:30:32 +0200

    flashlight flickering

--- Assets/Flashlights/flashlight_MrLarodos/flashlight_slow_parent.cs
+++ Assets/Flashlights/flashlight_MrLarodos/flashlight_slow_parent.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e542a6c1773ee5596cc289c8e53ccd75f8422ec1d6fd9527fcf09f577442787
-size 6586
+oid sha256:8ed8685ee3c5107bb4b02b80fa21573b6887ebb1da46c851f21e83514ad0fd06
+size 7377
