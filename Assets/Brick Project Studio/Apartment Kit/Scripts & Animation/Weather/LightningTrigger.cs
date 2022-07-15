c95060c2e30b2a809f2e8c91e3ebeeb11eaacda1
Mona Röhm <mona.roehm@protonmail.com>
2022-07-15 16:01:07 +0200

    blitz sound etc.

--- Assets/Brick Project Studio/Apartment Kit/Scripts & Animation/Weather/LightningTrigger.cs
+++ Assets/Brick Project Studio/Apartment Kit/Scripts & Animation/Weather/LightningTrigger.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55a5b91aaaa40eb1785ce968424a74da4eb135c28df2b7fd32a960760fe8c9f0
-size 643
+oid sha256:0933b364e1998998ae1173d241c5737fd490f5f551c4db0655a4fb8b3c3a348d
+size 1112
