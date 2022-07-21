ec46f6dcd36f870206f37986b49cec71f9dd88d6
Julian Dockendorf <julian.dockendorf@stud-mail.uni-wuerzburg.de>
2022-07-22 00:11:20 +0200

    open and close

--- Assets/Brick Project Studio/SE Basic Assets/Scripts and Animations/Doors/OpenAndClose.cs
+++ Assets/Brick Project Studio/SE Basic Assets/Scripts and Animations/Doors/OpenAndClose.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98a1fb485e2f7e8b58f7d66024c62f6ce8fdcfaa0009092e615b1ecb5e84aa67
-size 1861
+oid sha256:11c4f65002aef20af5ac74406409e2bc1e4f6d5cfa0f0cb8a066c7041913558a
+size 1994
