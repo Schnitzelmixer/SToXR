d3ce2b6485d0f668caf40cc06884685067c68d76
Julian Dockendorf <julian.dockendorf@stud-mail.uni-wuerzburg.de>
2022-07-23 16:18:29 +0200

    adjust lighting level

--- Assets/Brick Project Studio/Apartment Kit/Scripts & Animation/Light_flickering/all_lights.cs
+++ Assets/Brick Project Studio/Apartment Kit/Scripts & Animation/Light_flickering/all_lights.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ccab19bd81c0894ea766b4bf19e8de1b4c3ff73a821ec6f070b569e0b3c5369c
+oid sha256:726eb605e9141d67222971b2ad8aacd3ebb685935415b745bfa0999654a363cc
 size 3349
