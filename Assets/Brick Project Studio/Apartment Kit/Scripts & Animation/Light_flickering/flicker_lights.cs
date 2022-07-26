913bb90691a5e0d1ab86328be5d3462eb886a098
Julian Dockendorf <julian.dockendorf@stud-mail.uni-wuerzburg.de>
2022-07-26 18:28:06 +0200

    rename lights and glows

--- Assets/Brick Project Studio/Apartment Kit/Scripts & Animation/Light_flickering/flicker_lights.cs
+++ Assets/Brick Project Studio/Apartment Kit/Scripts & Animation/Light_flickering/flicker_lights.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0053b278247b339c16112a0bbcce5f9af9a979d73b0bff827305715df191cc01
-size 3792
+oid sha256:2be46e4676605916d6df8b99d98a2482c0f80caffbcb2d53419d8958fad69661
+size 4937
