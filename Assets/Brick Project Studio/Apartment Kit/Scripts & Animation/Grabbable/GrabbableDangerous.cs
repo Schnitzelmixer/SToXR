85c46da46e346360a4a1eca97b8136400a391821
Mona Röhm <mona.roehm@protonmail.com>
2022-07-26 23:41:32 +0200

    haptics and models

---
+++ Assets/Brick Project Studio/Apartment Kit/Scripts & Animation/Grabbable/GrabbableDangerous.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a1d841e9880228e026947741c6041ad8dd65bca0ba2a2a944b5ef51ec65cc100
+size 1239
---
+++ Assets/Brick Project Studio/Apartment Kit/Scripts & Animation/Grabbable/GrabbableWithSound.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:132755f17fd831866fd300f9a55a7d13683d91f42b1fa90d1849fe692958d930
+size 256
