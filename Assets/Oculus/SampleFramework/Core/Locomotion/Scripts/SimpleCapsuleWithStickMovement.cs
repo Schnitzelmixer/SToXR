10ab5fc9deb1c68c15b22e9ea52ed6dd5ae55bba
Mona <mona.roehm@protonmail.com>
2022-07-27 19:29:29 +0200

    lab stuff

--- Assets/Oculus/SampleFramework/Core/Locomotion/Scripts/SimpleCapsuleWithStickMovement.cs
+++ Assets/Oculus/SampleFramework/Core/Locomotion/Scripts/SimpleCapsuleWithStickMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af0d333fd7995274d920076afaee05e3e876e6c2b974a822b5fc8d0c80f4403e
-size 2649
+oid sha256:9d70ad1520046d62cfb00b1800d5381d7840203229a5d109b271f896a90cab12
+size 2651
