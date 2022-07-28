de28487aeaf4d8314923664b767017524bde83bf
Julian Dockendorf <julian.dockendorf@stud-mail.uni-wuerzburg.de>
2022-07-28 23:32:42 +0200

    gunshot fade

--- Assets/Brick Project Studio/Apartment Kit/Scripts & Animation/Managers/GameEventManager.cs
+++ Assets/Brick Project Studio/Apartment Kit/Scripts & Animation/Managers/GameEventManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a29186294baf06fe4f4a67f37d87d13cda1968ff0f70a68cf7b116641d8e297a
-size 3676
+oid sha256:d6e77091b8dba38c2e484b2ae5cf5b6a15b8f267e551577042ae53c19889aff9
+size 3894
---
+++ Assets/fade.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ce19ea5ce3998dac31542813788cd335654f5b064d9f7d2f73ba427a80e118f2
+size 593
