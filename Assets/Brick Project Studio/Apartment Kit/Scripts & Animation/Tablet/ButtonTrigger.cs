35d0a9e9feb0d96f37eac617c633b3f99c1e0ecc
Julian Dockendorf <julian.dockendorf@stud-mail.uni-wuerzburg.de>
2022-07-29 23:13:07 +0200

    tablet functionality

--- Assets/Brick Project Studio/Apartment Kit/Scripts & Animation/Tablet/ButtonTrigger.cs
+++ Assets/Brick Project Studio/Apartment Kit/Scripts & Animation/Tablet/ButtonTrigger.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a69d7c92226f394e2c879d47887a9c2d791a969cea7cc1997414f8d0505fc122
-size 489
+oid sha256:adbaa15cdcab031dee24a918f4f946b7141ab1bcac2979f478d5b4db0166474b
+size 378
---
+++ Assets/Brick Project Studio/Apartment Kit/Scripts & Animation/tablet_functionality.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:06e3877f3910002b4bf1efd20bc6bfecfc8f11291bf07bd6db0806907a72ecd0
+size 1805
