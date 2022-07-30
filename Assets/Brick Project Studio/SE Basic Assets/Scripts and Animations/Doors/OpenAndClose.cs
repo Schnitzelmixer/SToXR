91450e6b1bdb3f1da98d88cee1f976cd03c1ec19
Mona Röhm <mona.roehm@protonmail.com>
2022-07-30 02:25:04 +0200

    door sounds and structure layer

--- Assets/Brick Project Studio/SE Basic Assets/Scripts and Animations/Doors/OpenAndClose.cs
+++ Assets/Brick Project Studio/SE Basic Assets/Scripts and Animations/Doors/OpenAndClose.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:064e0fa344f71b04c97e4063e40bb5aec1e816d92a2a745245ca6bca70505d79
-size 1482
+oid sha256:fa4d13c18f75e99c85e26fb937ce08117f836c76968578ad26530c72511f5bc4
+size 1614
