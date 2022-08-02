b8a69c1429b99379b91dac1e923162dbcd146f6e
Julian Dockendorf <julian.dockendorf@stud-mail.uni-wuerzburg.de>
2022-08-02 19:50:35 +0200

    removed fadecanvas and instead added blood spatter

--- Assets/Brick Project Studio/Apartment Kit/Scripts & Animation/Grabbable/GrabbableDangerous.cs
+++ Assets/Brick Project Studio/Apartment Kit/Scripts & Animation/Grabbable/GrabbableDangerous.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7b8b3b155ee48ae3bb97ea0fad6d070d2e3fbafa8a55613df2905a23c29c448
-size 1362
+oid sha256:0aa79029516f4de80a6a1cca6f0e53b28b806dbc70a9202ce89e0a9fe3a821e2
+size 1295
--- Assets/Brick Project Studio/Apartment Kit/Scripts & Animation/TabletFunctionality.cs
+++ Assets/Brick Project Studio/Apartment Kit/Scripts & Animation/TabletFunctionality.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0123da935072b38af476f9a9043dfaa87c16525eb083b5834226db2afdb2036
-size 2176
+oid sha256:dbc89f5048fffe6d70595bb724e1f450bc4cd8badd9666f4de586ed87371f3bd
+size 2426
