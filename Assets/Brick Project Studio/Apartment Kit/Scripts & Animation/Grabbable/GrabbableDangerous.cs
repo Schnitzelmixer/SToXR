eed6bc9cf01428bc510037b1baf6c8014b21187f
Mona <mona.roehm@protonmail.com>
2022-08-03 18:31:50 +0200

    lab

--- Assets/Brick Project Studio/Apartment Kit/Scripts & Animation/Grabbable/GrabbableDangerous.cs
+++ Assets/Brick Project Studio/Apartment Kit/Scripts & Animation/Grabbable/GrabbableDangerous.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0aa79029516f4de80a6a1cca6f0e53b28b806dbc70a9202ce89e0a9fe3a821e2
-size 1295
+oid sha256:5ba7a71a4ff2976363ed10c23800f40ca9a8b6df39be35e7ff5eae0c8d8de93b
+size 1104
--- Assets/Brick Project Studio/Apartment Kit/Scripts & Animation/Managers/GameEventManager.cs
+++ Assets/Brick Project Studio/Apartment Kit/Scripts & Animation/Managers/GameEventManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16fadb2338ac5d0d4026f163ec6bd83147eccd19789c41bbf3e07b3617156119
-size 3969
+oid sha256:bcdeb328b0666f5de62d0b8d299749a5b64c3e98b091f22ce3a5c107ecc77825
+size 4051
