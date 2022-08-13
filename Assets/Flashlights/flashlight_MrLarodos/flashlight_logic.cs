7d9588beb6d050c02f05436581654c7eb70e6197
Julian Dockendorf <julian.dockendorf@stud-mail.uni-wuerzburg.de>
2022-08-13 18:08:14 +0200

    readme update

--- Assets/Flashlights/flashlight_MrLarodos/flashlight_logic.cs
+++ Assets/Flashlights/flashlight_MrLarodos/flashlight_logic.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:579f9f58dde8c2ded6804b2b9859f832fe3994d60216a931a902a4ebd330ea46
-size 3509
+oid sha256:ed19cbc820ade2b4fc65b6a494df89e59f18eb30148da9ea220d01590909bc08
+size 3007
